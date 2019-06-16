0ca0100d7654d0707c1654a6e0f2397f142aff98
Milosz Bauman <bauman.milosz@gmail.com>
2019-06-16 18:42:27 +0200

    Fixed mobile submenu

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
 const menuIcon = document.querySelector('.menu-icon');
 const nav = document.querySelector('.page-nav');
 menuIcon.addEventListener('click', () => {
-
   nav.classList.toggle('opened');
+})
+
+const subNavItem = document.querySelectorAll('.nav-list-item');
+
+subNavItem.forEach(el => {
+  el.addEventListener('click', () => {
+    const children = Array.from(el.children);
+    children.forEach(child => {
+      if(child.classList.contains('sub-list'))
+      child.style.display='block';
+    })
+  })
 })
 
 
@@ -10,6 +21,7 @@ const desktop = window.matchMedia("(min-width: 1024px)");
 const checkMedia = (e) => {
   if (e.matches) {
     nav.classList.remove('opened');
+    document.querySelectorAll('.sub-list').forEach(el => el.style.display = 'none');
   }
 }
 desktop.addListener(checkMedia);
